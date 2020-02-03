0797880cc4b0e89e6e60d07831bd67ae4efeb348
Gustavo <carneirinho@gmail.com>
2020-02-03 16:17:26 -0300

    Added Header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import './App.css';
+import Logo from './assets/logo-facebook.png';
+import Header from './components/Header';
+import PostList from './components/PostList'
+
 
 function App() {
-    return "Olá mundo";
+
+    const user = {
+        nome: 'Nicolas Cage',
+        avatar_url: 'https://www.placecage.com/26/26/',
+        face_link: 'https://facebook.com/ncage/'
+    }
+
+    return (
+        <div className="container">
+            <Header logo={Logo} user={user} />
+            <PostList />
+        </div>
+    )
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import propTypes from 'prop-types';
+
+function Header({logo, user}) {
+    return (
+        <header className='header'>
+            {logo && <a href='#'><img alt='Facebook' src={logo} style={{width: '121px', height: '24px'}}/></a>}
+            <div className="header__login">
+                <a href={user.face_link}>
+                    <p>{user.nome}</p>
+                    <img src={user.avatar_url} />
+                </a>
+            </div>
+        </header>
+    )
+}
+
+Header.defaultProps = {
+    user: {
+        nome: 'Usuário',
+        avatar_url: 'https://www.placecage.com/25/25/',
+        face_link: 'https://facebook.com/'
+    }
+}
+
+Header.propTypes = {
+    logo: propTypes.string.isRequired,
+    user: propTypes.object
+    
+}
+
+export default Header;
